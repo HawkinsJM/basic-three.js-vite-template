e487eebd2b1b882834be255bb896c724473a5a09
HawkinsJM <hawkins.jeffm@gmail.com>
2025-11-17 12:43:54 -0800

    moved camera

--- main.js
+++ main.js
@@ -19,10 +19,11 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+camera.position.y = 7;
+camera.lookAt(0, 0, 0);
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
 
   renderer.render(scene, camera);
 }
